9006f96ed4157c990164777b99149c361cb1b25b
ishak-dev <ishak.kazic@gmail.com>
2022-03-21 17:57:03 +0100

    FlightPHP

--- rest/index.php
+++ rest/index.php
@@ -1,11 +1,19 @@
 <?php
-require 'D:\xampp\htdocs\Web-Programming-2022\vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+require '../vendor/autoload.php';
 require_once "dao/server.php";
 
-Flight::route('/index.php',function(){
-    echo "Hello world!";
-});
+Flight::register('todoDao', 'TodoDao');
 
+Flight::route('/index.php','hello');
+
+function hello(){
+  echo "hello world!";
+}
 
 Flight::start();
 ?>
