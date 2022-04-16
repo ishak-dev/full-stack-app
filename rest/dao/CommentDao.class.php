f05e07653014d77f8f399f991d4439b17547fa7a
ishak-dev <ishak.kazic@gmail.com>
2022-04-16 18:25:33 +0200

    delete functions

--- rest/dao/CommentDao.class.php
+++ rest/dao/CommentDao.class.php
@@ -10,18 +10,7 @@ class CommentDao extends BaseDao{
   }
 
 
-  public function addUser($data){
-    $stmt = $this->conn->prepare("INSERT INTO $this->table_name (username, email) VALUES (:username, :email)");
-    $stmt->execute($data);
-    //$data['id'] = $this->conn->lastInsertId();
-    return $data;
-  }
-
-  public function updateUser($data){
-    $stmt = $this->conn->prepare("UPDATE $this->table_name SET username = :username, email = :email WHERE id = :id");
-    $stmt->execute($data);
-    return $data;
-  }
+  
 
 }
 
