8acfa33d19824d2e0a000c046cbd88009bef629a
ishak-dev <ishak.kazic@gmail.com>
2022-06-28 23:57:44 +0200

    swagger update

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -2,12 +2,16 @@
 
 
 /**
- * @OA\Info(title="ToDo API Specs", version="0.2", @OA\Contact(email="dino.keco@ibu.edu.ba", name="Dino Keco"))
+ * @OA\Info(title="ToDo API Specs", version="0.2", @OA\Contact(email="ishak.kazic@gmail.com", name="Ishak Kazic"))
  * @OA\OpenApi(
- *    @OA\Server(url="http://localhost/todo/rest", description="Development Environment" ),
+ *    @OA\Server(url="http://localhost/Web-Programming-2022/rest", description="Development Environment" ),
  *    @OA\Server(url="https://todos.biznet.ba/rest", description="Production Environment" )
  * ),
- * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
+ * @OA\SecurityScheme(
+ * securityScheme="ApiKeyAuth",
+ * type="apiKey",
+ * in="header",
+ * name="Authorization" )
  */
 
 ?>
